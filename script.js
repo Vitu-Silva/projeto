2c1e3f547a7b992bcc9fd02d6cbf05a8fe9e4e39
Vitu-Silva <seu-email>
2024-01-04 21:50:05 -0300

    Add pics and informations of Vitor

--- script.js
+++ script.js
@@ -9,11 +9,11 @@ function toggleMode() {
   // subistituir a imagem
   if (html.classList.contains("light")) {
     //se tiver light mode, adicionar a imagem light
-    img.setAttribute('src', './assets/avatar-light.png')
+    img.setAttribute('src', './assets/foto-light.png')
     img.setAttribute('alt', 'Foto de Mayk Brito sorrindo, usando óculos e jaqueta preta, barba e fundo azul e roxo')
   } else {
     // se tiver sem o light mode, manter a imagem normal
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "./assets/foto-dark.png")
     img.setAttribute("alt",'Foto de Mayk Brito sorrindo, usando óculos e camisa preta, barba e fundo amarelo"',)
   }
 }
